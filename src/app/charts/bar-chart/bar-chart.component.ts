fa2f6facb809ab05e02acddfd1afb8a068010ecc
weszty <weszty@yahoo.com>
2019-12-05 18:16:31 +0200

    finished the charts

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 const SAMPLE_BARCHART_DATA: any[] = [
-  { data: [65,59,80,81,56,54,30], label: 'Q3 Sales'},
-  { data: [25,39,6,91,36,54,50], label: 'Q4 Sales'}
+  { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
+  { data: [25, 39, 6, 91, 36, 54, 50], label: 'Q4 Sales'}
 ];
 
 const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
